6b099da3947b251153ae9372e119227c788e40a8
cameronly08 <cameron.ly@education.nsw.gov.au>
2023-08-09 10:34:07 +1000

    completed activity 6

--- 06.pulseWidthModulation/06.pulseWidthModulation.ino
+++ 06.pulseWidthModulation/06.pulseWidthModulation.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Cameron Ly
   Learning Intention: The students will learn what 'pulse width modulation' is and how to use it to write analogue data on a digital PIN.
   Success Criteria:
     1. I understand what pulse width modulation is
@@ -15,11 +15,20 @@
     https://www.tinkercad.com/things/drNDsMxGMIY?sharecode=D6x4_xTTRl_zGs7B-XGqfbQhG_xgjj3TVS_c-GNTK-c
     https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/06.pulseWidthModulation/Bootcamp-PWMOutput.png
 */
+unsigned static int lightSensor = A0;
+unsigned static int myLED = 5;
 
 void setup() {
-
+Serial.begin(9600);
+Serial.println("Serial monitor is configured to 9600");
+Serial.println("--------------");
+pinMode (myLED,OUTPUT);
 }
 
 void loop() {
-
+ int val = analogRead(lightSensor);
+Serial.print("LightSensor:");
+Serial.println(val);
+int LEDbright = 255;
+analogWrite(myLED,LEDbright); 
 }
